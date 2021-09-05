83eb963f101862609619f0b3357dc3b819300d3a
eru123 <yeoligoakino@gmail.com>
2021-09-06 03:52:13 +0800

    added: customization

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const _ = require('lodash')
 
 
 const plugin = async (fastify, options, done) => {
+    const name = options.name || 'tuos'
     const globalOpts = options.options || {}
+    const custom = options.custom || {}
     const excluded = options.excluded || []
     const last = options.last || []
     const order = options.order || []
-    const plugins = {}
+    const only = options.only || []
+    let plugins = {}
     const dir = path.resolve(options.dir) 
     const raw = _.omit(autoload(dir), excluded)
     for (const key in raw) if ('index' in raw[key] && 'plugin' in raw[key].index) plugins[key] = raw[key].index
@@ -35,20 +38,27 @@ const plugin = async (fastify, options, done) => {
         }
     }
 
+    if (only.length > 0) plugins = _.pick(plugins, only)
+
     const installOrder = async (orderList) => {
         if(!(typeof orderList === 'object')) throw new Error(`[PLUGIN] 'order' must be an array`)
         if(!Array.isArray(orderList)) throw new Error(`[PLUGIN] 'order' must be an array`)
         orderList.forEach(async (key) => {
-            if(typeof key === 'string') await install(key)
-            else await installOrder(key)
+            if(typeof key === 'string' && installed.indexOf(key) === -1) await install(key)
+            else if (Array.isArray(key)) await installOrder(key)
         })
     }
 
     await installOrder(order)
 
-    for(const key in plugins) if (last.indexOf(key) > -1) await install(key)
-    for(const key in plugins) await install(key)
+    for(const key in _.omit(plugins,installed)) if (last.indexOf(key) > -1) await install(key)
+    for(const key in  _.omit(plugins,installed)) await install(key)
     
+    if(name in fastify) {
+        for(const key in plugins) fastify[name].plugins[key] = plugins[key]
+        for(const key in globalOpts) fastify[name].options[key] = globalOpts[key]
+    } else fastify.decorate(name, {plugins, options: globalOpts, custom})
+
     done()
 }
 
